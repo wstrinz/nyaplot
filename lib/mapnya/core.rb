38dea98d0ec799e027e71c44a54f2e293fd55cfd
Will Strinz <wstrinz@gmail.com>
2016-02-07 00:52:42 -0600

    working mapnya

--- lib/mapnya/core.rb
+++ lib/mapnya/core.rb
@@ -1,5 +1,5 @@
 module Nyaplot
   add_extension("Mapnya")
-  add_additional_library("Mapnya","http://cdn.rawgit.com/domitry/nyaplot/master/lib/mapnya/js/release/mapnya")
+  add_additional_library("Mapnya","https://rawgit.com/wstrinz/nyaplot/js-es6/lib/mapnya/js/dist/mapnya.iife")
   init_iruby if defined? IRuby
 end
